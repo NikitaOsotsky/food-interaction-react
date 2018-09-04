be44cbd7015361548e5cca2866ea6a5d93911318
Mikita Asotski <masotski@exadel.com>
2018-09-04 15:52:57 +0300

    add rendering; add exeptions to static clickHandler;

--- food-interaction/src/components/main-menu/MainMenu.js
+++ food-interaction/src/components/main-menu/MainMenu.js
@@ -1,11 +1,22 @@
 import { Component } from "react";
 import React from "react";
 import './MainMenu.css'
+import SubMenu from '../sub-menu/SubMenu';
+import * as ReactDOM from "react-dom";
 
 class MainMenu extends Component {
 
-  clickHandler() {
-
+  static clickHandler(e) {
+    if(e.target.tagName !== 'LI' && e.target.parentElement.tagName !== 'LI') {
+      return;
+    }
+    if(e.target.tagName !== 'LI' && e.target.parentElement.tagName === 'LI') {
+      e.target = e.target.parentElement;
+      this.clickHandler(e);
+    } else {
+      console.log('YEE!');
+      ReactDOM.render(<SubMenu target={e.target}/>, document.querySelector('.sub-menu'));
+    }
   }
 
   render() {
@@ -13,11 +24,11 @@ class MainMenu extends Component {
     const listItems = items.map((item) =>
       <li key={item.id} id={item.id} className="main-menu__list__element">
         <img className="main-menu__list__element__logo" src={item.image} alt={item.name}/>
-        <label className="main-menu__list__element__text">{item.name}</label>
+        <label id={item.id} className="main-menu__list__element__text">{item.name}</label>
       </li>
     );
     return (
-        <ul onClick={() => this.clickHandler()} className="main-menu__list">
+        <ul onClick={(e) => MainMenu.clickHandler(e)} className="main-menu__list">
           {listItems}
         </ul>
     );
