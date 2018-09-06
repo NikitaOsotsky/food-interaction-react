09393a3e46fced35f0af7dfcef2fd97555a3eb0f
Mikita Asotski <masotski@exadel.com>
2018-09-06 11:21:11 +0300

    add new module SubMenuChooser for saving choose status and chosen objects;

---
+++ food-interaction/src/components/sub-menu/sub-menu-chooser/SubMenuChooser.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import React from "react";
+
+class SubMenuChooser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chosenObjects: []
+    };
+  }
+}
+
+export default SubMenuChooser;
